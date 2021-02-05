c8226ff95dc52632f9d0af2e2e3b8c49be49c3bd
Adam Steinberg <adam.steinberg@postgrad.manchester.ac.uk>
2021-02-05 14:05:49 +0000

    Settings File Introduced
    * Now have a settings file, first thing to go in there is a string giving the directory of fieldmaps.
        * This file will be added to the gitignore so must be updated *manually*

--- param_io.py
+++ param_io.py
@@ -14,16 +14,19 @@ theta_f  : Angle covered by an F magnet
 (Note that in the original lattice, the F and D parameters all had the same magnitude, with appropriate sign.)
 B0_d     : Reference field of the D magnet
 B0_f     : Reference field of the F magnet
-r0_d     : Reference radius of the D magnet
-r0_f     : Reference radius of the F magnet
 k_d      : Field index of the D magnet
 k_f      : Field index of the F magnet
+r0       : Reference radius of the magnets  # TODO: What about offsets??
 
 ##MISC:
 N_cell   : Total number of cells for modelling
+name     : Name to give the lattice in tests etc.
+r_min    : Minimum orbit radius (for display)
+r_max    : Maximum orbit radius (for display)
 
 All angles are stored in degrees, distances in cm, magnetic fields in T
 """
+import json
 
 def get_default_params():
     """A simple script to get the (approx) params used in the actual FFA, without worrying about file io."""
@@ -32,11 +35,23 @@ def get_default_params():
                       theta_d=3.43,
                       theta_sd=1.7,
                       theta_f=10.24,
+
                       B0_d=-1.6,
                       B0_f=1.6,
-                      r0_d=540,
-                      r0_f =540,
+                      r0=540,
                       k_d=7.7,
                       k_f=7.7,
-                      N_cell=1)
+
+                      N_cell=1,
+                      name="Default KURRI Cell",
+                      r_min=485.4,
+                      r_max=540)
     return param_dict
+
+def get_fieldmap_dir():
+    """Utility function to read the fieldmap directory from the settings file.
+    Likely not a permanent function, better in future to return a cached property"""
+    with open("settings.json") as settings:
+        data = json.load(settings)
+        fieldmap_dir = data["fieldmapDir"]
+        return fieldmap_dir
